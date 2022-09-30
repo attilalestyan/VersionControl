af7297c152f1b3134382987caa4e58bd3645d46e
Lestyán Attila <lesattila@gmail.com>
2022-09-30 09:49:40 +0200

    hihi

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -6,20 +6,10 @@ using System.Threading.Tasks;
 
 namespace UserMaintenance.Entities
 {
-    public class User
+    class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName
-        {
-            get
-            {
-                return string.Format(
-                    "{0} {1}",
-                    LastName,
-                    FirstName);
-            }
-        }
-    }
+
+        public string FullName { get; set; }
+    }  
 }
