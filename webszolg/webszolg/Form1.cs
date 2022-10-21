4c26b28eb29051882cfc0fdce39441ccd942353f
Attila Lestyán <attila.lestyan@stud.uni-corvinus.hu>
2022-10-21 14:04:26 +0200

    1, 2, 3, 4

---
+++ webszolg/webszolg/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using webszolg.MnbServiceReference;
+using webszolg.Entities;
+
+namespace webszolg
+{
+    public partial class Form1 : Form
+    {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        public Form1()
+        {
+            InitializeComponent();
+            GetExchangeRates();
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
